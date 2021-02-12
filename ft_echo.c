af6ddf449dfc05ed6fe7c41788a2e16aeb1f2a4b
fmoaney <fmoaney@student.21-school.ru>
2021-02-12 19:02:19 +0300

    fixed echo with

--- ft_echo.c
+++ ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: fmoaney <fmoaney@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/26 21:46:00 by fmoaney           #+#    #+#             */
-/*   Updated: 2021/02/07 13:10:57 by fmoaney          ###   ########.fr       */
+/*   Updated: 2021/02/12 18:56:34 by fmoaney          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,26 @@
 int	ft_echo(char **args)
 {
 	int fl_n;
+	int	fl_f;
 
 	fl_n = 0;
-	if (args == NULL)
-		return (-1);
-	while (*args && ft_strequal(*args, "-n"))
-	{
+	fl_f = 0;
+	while (*args && ft_strequal(*args, "-n") && (fl_n = 1))
 		args++;
-		fl_n = 1;
-	}
 	while (*args)
 	{
-		if (ft_puts(*args++) == -1)
-			return (errno);
-		if (*args != NULL)
-			if (ft_putchar_fd(' ', 1) == -1)
+		if (**args != '\0')
+		{
+			if (fl_f && ft_putchar_fd(' ', 1) == -1)
 				return (errno);
+			else
+				fl_f = 1;
+			if (ft_puts(*args) == -1)
+				return (errno);
+		}
+		args++;
 	}
-	if (!fl_n)
-		if (ft_putchar_fd('\n', 1) == -1)
-			return (errno);
+	if (!fl_n && ft_putchar_fd('\n', 1) == -1)
+		return (errno);
 	return (0);
 }
